0ed4649d654ba341ba19becf923e0d8c6ccd2e95
jmanuelcorral <jose.corral@tokiota.com>
2017-02-16 13:03:20 +0100

    First Commit!

---
+++ Tokiota.Middlewares/Tokiota.Middlewares.Web.Tests/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿namespace Tokiota.Middlewares.Web.Tests
+{
+    using Xunit;
+    using System.Threading.Tasks;
+
+    public class HelloWorld
+    {
+        public HelloWorld()
+        {
+        }
+
+        [Fact]
+        public async Task TestHelloWorldMiddlewareWorks()
+        {
+            // Act
+            // Arrange 
+            // Assert
+           /* esperaremos algo así como....  Assert.True(responseString.Contains("Hello World!")); */
+        }
+    }
+}
